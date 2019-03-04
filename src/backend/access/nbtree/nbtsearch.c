9990148107fda60568fb3102393dad090336aa58
erthalion <9erthalion6@gmail.com>
2019-03-04 23:37:18 +0100

    Fix for backward skip

--- src/backend/access/nbtree/nbtsearch.c
+++ src/backend/access/nbtree/nbtsearch.c
@@ -1501,8 +1501,76 @@ _bt_skip(IndexScanDesc scan, ScanDirection dir, int prefix)
 					   scan->xs_snapshot);
 	_bt_freestack(stack);
 	so->currPos.buf = buf;
-	offnum = _bt_binsrch(scan->indexRelation, buf, prefix, so->skipScanKey,
-						 ScanDirectionIsForward(dir));
+	if (ScanDirectionIsForward(dir))
+	{
+		offnum = _bt_binsrch(scan->indexRelation, buf, prefix, so->skipScanKey,
+							 ScanDirectionIsForward(dir));
+	}
+	else
+	{
+		bool found;
+		TupleDesc	itupdesc;
+		int			indnkeyatts;
+		int i;
+
+		offnum = _bt_binsrch(scan->indexRelation, buf, prefix, so->skipScanKey,
+							 ScanDirectionIsForward(dir));
+
+		found = _bt_next(scan, dir);
+
+		itupdesc = RelationGetDescr(indexRel);
+		indnkeyatts = IndexRelationGetNumberOfKeyAttributes(indexRel);
+		for (i = 0; i < indnkeyatts; i++)
+		{
+			Datum datum;
+			bool null;
+			int flags;
+
+			datum = index_getattr(scan->xs_itup, i + 1, itupdesc, &null);
+			flags = (null ? SK_ISNULL : 0) |
+					(indexRel->rd_indoption[i] << SK_BT_INDOPTION_SHIFT);
+			so->skipScanKey[i].sk_flags = flags;
+			so->skipScanKey[i].sk_argument = datum;
+		}
+
+		/*offnum = _bt_binsrch_tmp(scan->indexRelation, buf, prefix, so->skipScanKey,*/
+							 /*ScanDirectionIsForward(dir));*/
+		/* Check if the next unique key can be found within the current page */
+		if (BTScanPosIsValid(so->currPos))
+		{
+			buf = so->currPos.buf;
+
+			page = BufferGetPage(buf);
+			opaque = (BTPageOpaque) PageGetSpecialPointer(page);
+
+			low = P_FIRSTDATAKEY(opaque);
+			high = PageGetMaxOffsetNumber(page);
+			compare_offset = ScanDirectionIsForward(dir) ? high : low;
+
+			if(_bt_compare(scan->indexRelation, prefix,
+						   so->skipScanKey, page, compare_offset) > compare_value)
+			{
+				bool keyFound = false;
+
+				LockBuffer(buf, BT_READ);
+				offnum = _bt_binsrch(scan->indexRelation, buf, prefix, so->skipScanKey,
+									 ScanDirectionIsForward(dir));
+			}
+		}
+
+		/*
+		 * We haven't found scan key within the current page, so let's scan from
+		 * the root. Use _bt_search and _bt_binsrch to get the buffer and offset
+		 * number
+		 */
+		stack = _bt_search(scan->indexRelation, prefix, so->skipScanKey,
+						   ScanDirectionIsForward(dir), &buf, BT_READ,
+						   scan->xs_snapshot);
+		_bt_freestack(stack);
+		so->currPos.buf = buf;
+			offnum = _bt_binsrch(scan->indexRelation, buf, prefix, so->skipScanKey,
+								 ScanDirectionIsForward(dir));
+	}
 
 	/* Lock the page for SERIALIZABLE transactions */
 	PredicateLockPage(scan->indexRelation, BufferGetBlockNumber(buf),
@@ -1515,6 +1583,10 @@ _bt_skip(IndexScanDesc scan, ScanDirection dir, int prefix)
 	{
 		/* Move back for _bt_next */
 		offnum = OffsetNumberPrev(offnum);
+	}
+	else
+	{
+		offnum = OffsetNumberNext(offnum);
 	}
 
 	/* Now read the data */
