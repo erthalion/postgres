666a8095b700365f78de80fa0febc4a0ac24ae7a
jesperpedersen <jesper.pedersen@redhat.com>
2019-11-15 09:46:53 -0500

    Index skip scan
    Implementation of Index Skip Scan (see Loose Index Scan in the wiki [1])
    on top of IndexOnlyScan and IndexScan. To make it suitable for both
    situations when there are small number of distinct values and
    significant amount of distinct values the following approach is taken -
    instead of searching from the root for every value we're searching for
    then first on the current page, and then if not found continue searching
    from the root.
    
    Original patch and design were proposed by Thomas Munro [2], revived and
    improved by Dmitry Dolgov and Jesper Pedersen.
    
    [1] https://wiki.postgresql.org/wiki/Loose_indexscan
    [2] https://www.postgresql.org/message-id/flat/CADLWmXXbTSBxP-MzJuPAYSsL_2f0iPm5VWPbCvDbVvfX93FKkw%40mail.gmail.com
    
    Author: Jesper Pedersen, Dmitry Dolgov
    Reviewed-by: Thomas Munro, David Rowley, Floris Van Nee, Kyotaro Horiguchi, Tomas Vondra, Peter Geoghegan

--- src/backend/optimizer/path/indxpath.c
+++ src/backend/optimizer/path/indxpath.c
@@ -791,6 +791,16 @@ get_index_paths(PlannerInfo *root, RelOptInfo *rel,
 	{
 		IndexPath  *ipath = (IndexPath *) lfirst(lc);
 
+		/*
+		 * To prevent unique paths from index skip scans being potentially used
+		 * when not needed scan keep them in a separate pathlist.
+		*/
+		if (ipath->indexskipprefix != 0)
+		{
+			add_unique_path(rel, (Path *) ipath);
+			continue;
+		}
+
 		if (index->amhasgettuple)
 			add_path(rel, (Path *) ipath);
 
@@ -880,6 +890,8 @@ build_index_paths(PlannerInfo *root, RelOptInfo *rel,
 	bool		pathkeys_possibly_useful;
 	bool		index_is_ordered;
 	bool		index_only_scan;
+	bool		not_empty_qual;
+	bool		can_skip;
 	int			indexcol;
 
 	/*
@@ -1029,6 +1041,17 @@ build_index_paths(PlannerInfo *root, RelOptInfo *rel,
 	index_only_scan = (scantype != ST_BITMAPSCAN &&
 					   check_index_only(rel, index));
 
+	/* Check if an index skip scan is possible. */
+	can_skip = enable_indexskipscan & index->amcanskip;
+
+	/*
+	 * In case of index scan (not index-only scan) skip scan is not supported
+	 * when there are qual conditions present. Check if they are.
+	 */
+	not_empty_qual = (root->parse->jointree != NULL &&
+					  root->parse->jointree->quals != NULL &&
+					  list_length((List *) root->parse->jointree->quals) != 0);
+
 	/*
 	 * 4. Generate an indexscan path if there are relevant restriction clauses
 	 * in the current clauses, OR the index ordering is potentially useful for
@@ -1055,6 +1078,13 @@ build_index_paths(PlannerInfo *root, RelOptInfo *rel,
 								  loop_count,
 								  false);
 		result = lappend(result, ipath);
+
+		/* Consider index skip scan as well */
+		if (useful_uniquekeys != NULL && can_skip &&
+			(index_only_scan || !not_empty_qual))
+			result = lappend(result,
+							 create_skipscan_unique_path(root, index,
+								 						 (Path *) ipath));
 
 		/*
 		 * If appropriate, consider parallel index scan.  We don't allow
@@ -1115,6 +1145,13 @@ build_index_paths(PlannerInfo *root, RelOptInfo *rel,
 									  loop_count,
 									  false);
 			result = lappend(result, ipath);
+
+			/* Consider index skip scan as well */
+			if (useful_uniquekeys != NULL && can_skip &&
+				(index_only_scan || !not_empty_qual))
+				result = lappend(result,
+								 create_skipscan_unique_path(root, index,
+															 (Path *) ipath));
 
 			/* If appropriate, consider parallel index scan */
 			if (index->amcanparallel &&
