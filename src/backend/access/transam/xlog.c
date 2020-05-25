9234e1a8c5de4f5fb694687d310a822fee9f169d
Andres Freund <andres@anarazel.de>
2020-05-25 03:02:56 -0700

    aio: xlog: small fixes
    Author:
    Reviewed-By:
    Discussion: https://postgr.es/m/
    Backpatch:

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -10464,8 +10464,19 @@ issue_xlog_fsync(int fd, XLogSegNo segno)
 	switch (sync_method)
 	{
 		case SYNC_METHOD_FSYNC:
+#if 1
+			if (enableFsync)
+			{
+				PgAioInProgress *aio = pgaio_io_get();
+
+				pgaio_start_fsync(aio, fd, false);
+				pgaio_wait_for_io(aio, true);
+				pgaio_release(aio);
+			}
+#else
 			if (pg_fsync_no_writethrough(fd) != 0)
 				msg = _("could not fsync file \"%s\": %m");
+#endif
 			break;
 #ifdef HAVE_FSYNC_WRITETHROUGH
 		case SYNC_METHOD_FSYNC_WRITETHROUGH:
@@ -10480,7 +10491,7 @@ issue_xlog_fsync(int fd, XLogSegNo segno)
 			{
 				PgAioInProgress *aio = pgaio_io_get();
 
-				pgaio_start_fdatasync(aio, fd, true);
+				pgaio_start_fdatasync(aio, fd, false);
 				pgaio_wait_for_io(aio, true);
 				pgaio_release(aio);
 			}
@@ -10492,13 +10503,6 @@ issue_xlog_fsync(int fd, XLogSegNo segno)
 #endif
 		case SYNC_METHOD_OPEN:
 		case SYNC_METHOD_OPEN_DSYNC:
-			{
-				PgAioInProgress *aio = pgaio_io_get();
-
-				pgaio_start_fdatasync(aio, fd, true);
-				pgaio_wait_for_io(aio, true);
-				pgaio_release(aio);
-			}
 			/* write synced it already */
 			break;
 		default:
