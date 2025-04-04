01f5852a15346190ce0f501278af153c904b6745
Dmitrii Dolgov <9erthalion6@gmail.com>
2025-04-04 21:46:14 +0200

    Introduce pss_barrierReceivedGeneration
    Currently WaitForProcSignalBarrier allows to make sure the message sent
    via EmitProcSignalBarrier was processed by all ProcSignal mechanism
    participants.
    
    Add pss_barrierReceivedGeneration alongside with pss_barrierGeneration,
    which will be updated when a process has received the message, but not
    processed it yet. This makes it possible to support a new mode of
    waiting, when ProcSignal participants want to synchronize message
    processing. To do that, a participant can wait via
    WaitForProcSignalBarrierReceived when processing a message, effectively
    making sure that all processes are going to start processing
    ProcSignalBarrier simultaneously.

--- src/include/storage/procsignal.h
+++ src/include/storage/procsignal.h
@@ -79,6 +79,7 @@ extern void SendCancelRequest(int backendPID, char *cancel_key, int cancel_key_l
 
 extern uint64 EmitProcSignalBarrier(ProcSignalBarrierType type);
 extern void WaitForProcSignalBarrier(uint64 generation);
+extern void WaitForProcSignalBarrierReceived(uint64 generation);
 extern void ProcessProcSignalBarrier(void);
 
 extern void procsignal_sigusr1_handler(SIGNAL_ARGS);
